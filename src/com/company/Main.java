540a48f76ad016d0aea1e62de9b4580db6ca0351
ergunjon <ergunjon@gmail.com>
2021-01-26 17:55:50 -0700

    Solar system ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.sun=1;
+        FactorX.planets=2;
+        FactorX.Moons=3;
+    }
+}
